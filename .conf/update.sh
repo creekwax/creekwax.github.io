ef35800a7026e7ef0fba22b0e332c35371c8e115
wangxiaoxi <creekwax@gmail.com>
2018-12-06 01:09:22 +0800

    Add lost in economics post.

---
+++ .conf/update.sh
@@ -0,0 +1,4 @@
+mkdir -p creekwax.github.io/.md/ creekwax.github.io/.conf/
+cp -r source/* creekwax.github.io/.md/
+cp _config.yml update.sh creekwax.github.io/.conf/
+cp -r public/* creekwax.github.io/
